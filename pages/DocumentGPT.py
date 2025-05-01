24b5e0f834de198625f00101d6f9afcf4a9d7f17
0gunkim <goldenprevue@gmail.com>
2025-05-01 21:13:59 +0900

    Feat: Document GPT

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -1,24 +1,134 @@
 import streamlit as st
+import os
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain.vectorstores import FAISS
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks.base import BaseCallbackHandler
+
+st.set_page_config(page_title="Document GPT", page_icon="📰")
+
+# API 키 입력 받기
+with st.sidebar:
+    api_key = st.text_input("OpenAI API 키를 입력하세요", type="password", key="api_key")
+    if st.button("Delete Key"):
+        st.session_state.api_key = ""
+        os.environ["OPENAI_API_KEY"] = ""
+        st.rerun()
+
+if not api_key:
+    st.error("OpenAI API 키를 입력해주세요.")
+    st.stop()
+
+os.environ["OPENAI_API_KEY"] = api_key
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+llm = ChatOpenAI(
+    model_name="gpt-4o-mini-2024-07-18",
+    temperature=0.1,
+    streaming=True,
+    callbacks=[ChatCallbackHandler()]
+)
+
+
+@st.cache_resource(show_spinner="Embedding...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = TextLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    # embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+prompt = ChatPromptTemplate.from_messages(
+[
+    (
+        "system",
+        """
+        Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+        
+        Context: {context}
+        """,
+    ),
+    ("human", "{question}"),
+]
+)
 
 st.title("Document GPT")
+st.markdown("""
+## Hi!
+##### 파일을 업로드하면,
+##### 파일의 내용을 분석하고,
+##### 질문에 답변해드리는 기능입니다.
+""")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+with st.sidebar:
+    file = st.file_uploader("Upload a file Only .ttxt", type=["txt"])
 
-def send_message(message, role, save):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"role": role, "message": message})
 
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
-
-message = st.chat_input("Ask a question about the document")
-
-if message:
-    send_message(message, "human", save=True)
-    send_message(f"{message}", "ai", save=True)
-
-# with st.sidebar:
-#     st.write(st.session_state)
+if file:
+    retriever = embed_file(file)
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+            send_message(message, "human")
+            chain = (
+                {
+                    "context": retriever | RunnableLambda(format_docs),
+                    "question": RunnablePassthrough(),
+                }
+                | prompt
+                | llm
+            )
+            with st.chat_message("ai"):
+                response = chain.invoke(message)
+else:
+    st.session_state["messages"] = []
